d7e5c3c638398df6f0f7684ba87015ee4e6e78de
Christopher Boo <christopherboo66@gmail.com>
2017-07-25 08:27:40 +0800

    solved problem 7

---
+++ 7.hs
@@ -0,0 +1,6 @@
+primes = 2 : primes'
+    where isPrime (p:ps) n = p * p > n || n `rem` p /= 0 && isPrime ps n
+          primes' = 3 : filter (isPrime primes') [3,5..]
+
+main = do
+    print $ primes !! 10001
