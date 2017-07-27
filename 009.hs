cf41028e9476ab490801c22a72d7e7974dce48f7
Christopher Boo <christopherboo66@gmail.com>
2017-07-27 20:44:38 +0800

    use `head`

--- 009.hs
+++ 009.hs
@@ -1,2 +1,2 @@
 main = do
-    print $ [a * b * c | c <- [335..499], b <- [((1000-c) `quot` 2)..(c-1)], let a = 1000 - b - c, c * c == a * a + b * b]
+    print $ head [a * b * c | c <- [335..499], b <- [((1000-c) `quot` 2)..(c-1)], let a = 1000 - b - c, c * c == a * a + b * b]
