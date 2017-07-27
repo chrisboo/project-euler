d560bd6edabc35ca2f058e77eae198ae6c813e98
Christopher Boo <christopherboo66@gmail.com>
2017-07-27 20:31:14 +0800

    replace if/else to guards

--- 003.hs
+++ 003.hs
@@ -3,13 +3,11 @@ primes = 2 : primes'
     where isPrime (p:ps) n = p * p > n || n `rem` p /= 0 && isPrime ps n
           primes' = 3 : filter (isPrime primes') [5, 7 ..]
 
-largestPrimeFactor :: Integral a => [a] -> a -> a
-largestPrimeFactor (p:ps) n = 
-    if n == p 
-        then p 
-    else if n `rem` p == 0 
-        then largestPrimeFactor (p:ps) (quot n p) 
-    else largestPrimeFactor ps n
+largestPrimeFactor :: (Integral a) => [a] -> a -> a
+largestPrimeFactor (p:ps) n
+    | n == p         = p 
+    | n `rem` p == 0 = largestPrimeFactor (p:ps) (quot n p) 
+    | otherwise      = largestPrimeFactor ps n
 
 main = do
     print $ largestPrimeFactor primes 600851475143
